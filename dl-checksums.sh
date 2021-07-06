8119e6c692436fdeb31877d87959734f4e689822
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-07-06 17:37:30 -0400

    kubelogin:0.0.9

---
+++ dl-checksums.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+set -e
+
+DIR=~/Downloads
+MIRROR=https://github.com/Azure/kubelogin/releases/download
+
+# https://github.com/Azure/kubelogin/releases/download/v0.0.9/kubelogin-linux-amd64.zip
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-zip}
+    local platform="${os}-${arch}"
+    local url=$MIRROR/v$ver/kubelogin-${platform}.${archive_type}
+    local lfile=$DIR/kubelogin-${platform}-${ver}.${archive_type}
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver win amd64
+}
+
+dl_ver ${1:-0.0.9}
