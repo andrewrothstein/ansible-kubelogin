9aadddf1e25ab68e29359f6f09f4902f32f5a0a2
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-02-28 09:33:41 -0500

    kubelogin:0.0.11

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -13,22 +13,19 @@ dl()
     local arch=$3
     local archive_type=${4:-zip}
     local platform="${os}-${arch}"
-    local url=$MIRROR/v$ver/kubelogin-${platform}.${archive_type}
-    local lfile=$DIR/kubelogin-${platform}-${ver}.${archive_type}
-    if [ ! -e $lfile ];
-    then
-        wget -q -O $lfile $url
-    fi
+    local url=$MIRROR/v$ver/kubelogin-${platform}.${archive_type}.sha256
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
     dl $ver darwin amd64
+    dl $ver darwin arm64
     dl $ver linux amd64
+    dl $ver linux arm64
     dl $ver win amd64
 }
 
-dl_ver ${1:-0.0.10}
+dl_ver ${1:-0.0.11}
